f2164cbec33876761cf1ae5080228d2a3c1a3415
shahed <shahed.rahman88@gmail.com>
2022-03-27 22:06:35 +0600

    added routes

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,11 +3,18 @@ import Menu from "./Menu";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
+import { Route, Redirect } from "react-router-dom";
+import { Routes } from "react-router";
 
 const Body = () => {
     return (
         <div>
-
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/menu" exact element={<Menu />} />
+                <Route path="/contact" exact element={<Contact />} />
+                <Route path="/about" exact element={<About />} />
+            </Routes>
         </div>
     )
 }
--- src/components/body/Contact.js
+++ src/components/body/Contact.js
@@ -5,7 +5,7 @@ class Contact extends Component {
     render() {
         return (
             <div>
-
+                contact
             </div>
         )
     }
