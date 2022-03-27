5a1b6e0e634c6ba0ce83fda197f4b5e9aa085c56
shahed <shahed.rahman88@gmail.com>
2022-03-27 21:51:36 +0600

    nav collapse added

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -3,27 +3,43 @@ import {Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse} from 'reacts
 import { Link } from "react-router-dom";
 
 class Navigation extends Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+    }
+
+    navToggle = () => {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+    
     render() {
         return (
             <Navbar dark color="dark" expand="sm">
                 {/* <div className="container"> */}
+                <NavbarToggler onClick={this.navToggle} />
                     <NavbarBrand href="/">
                         Bohubrihi Restaurant
                     </NavbarBrand>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <Link to="/" className="nav-link active">Home</Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link to="/menu" className="nav-link">Menu</Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link to="/about" className="nav-link">About</Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link to="/contact" className="nav-link">Contact</Link>
-                        </NavItem>
-                    </Nav>
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Link to="/" className="nav-link active">Home</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/menu" className="nav-link">Menu</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/about" className="nav-link">About</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/contact" className="nav-link">Contact</Link>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
                 {/* </div> */}
             </Navbar>
         )
