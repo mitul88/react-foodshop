e4ccd80e0ae5b1408efb008b123689d4fedf58eb
shahed <shahed.rahman88@gmail.com>
2022-03-27 20:58:45 +0600

    nav-items added

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,15 +1,30 @@
 import React, {Component} from "react";
-import {Navbar, NavbarBrand} from 'reactstrap';
+import {Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse} from 'reactstrap';
+import { Link } from "react-router-dom";
 
 class Navigation extends Component  {
     render() {
         return (
-            <Navbar dark color="dark">
-                <div className="container">
+            <Navbar dark color="dark" expand="sm">
+                {/* <div className="container"> */}
                     <NavbarBrand href="/">
                         Bohubrihi Restaurant
                     </NavbarBrand>
-                </div>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Link to="/" className="nav-link active">Home</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/menu" className="nav-link">Menu</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/about" className="nav-link">About</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/contact" className="nav-link">Contact</Link>
+                        </NavItem>
+                    </Nav>
+                {/* </div> */}
             </Navbar>
         )
     }
