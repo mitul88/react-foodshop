4f91c135ce6a1de55fa4568cbbc0da386dcdd4e5
shahed <shahed.rahman88@gmail.com>
2022-04-25 22:12:03 +0600

    mock api passed as initail state

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,11 @@
+import DISHES from "../data/dishes";
+import COMMENTS from "../data/comments";
+
+const initialState = {
+    dishes: DISHES,
+    comments: COMMENTS,
+}
+
 const reducer = (state, action) => {
     return state;
 }
