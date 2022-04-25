0598cdf7c446cc526e721767afa8c4e9cbe75317
shahed <shahed.rahman88@gmail.com>
2022-04-26 00:10:18 +0600

    cleaned up home component

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
-    console.log(state)
     return {
         dishes: state.dishes
     }
@@ -10,10 +9,6 @@ const mapStateToProps = state => {
 
 
 class Home extends Component {
-    componentDidMount() {
-        console.log("Home state: " + this.state);
-        console.log( this.props);
-    }
     render() {
         document.title="Home";
         return (
